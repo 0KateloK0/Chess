821d3111aaa92a3b59b5a28b8fb3b7a701362ca9
0KateloK0 <48626591+0KateloK0@users.noreply.github.com>
2019-05-10 15:24:22 +0300

    Limits

--- main.js
+++ main.js
@@ -3,6 +3,30 @@
 		// initialization
 		var field = [];
 
+		function find_enemy_king () {
+			for (var i = 0; i < 8; i++)
+				for (var j = 0; j < 8; j++)
+					if ((field[i][j] instanceof King) && (field[i][j].color != order_now)) 
+						return {x: j, y: i};
+		}
+
+		function check_enemy() {
+			var kp = find_enemy_king();
+			return field.some( (a) => a.some( (b) => b.check(kp.x, kp.y) ) );
+		}
+
+		function find_union_king() {
+			for (var i = 0; i < 8; i++)
+				for (var j = 0; j < 8; j++)
+					if ((field[i][j] instanceof King) && (field[i][j].color == order_now)) 
+						return {x: j, y: i};
+		}
+
+		function check_union() {
+			var kp = find_union_king();
+			return field.some( (a) => a.some( (b) => b.check(kp.x, kp.y) ) );
+		}
+
 		// система наследования: от класса фигуры наследуют классы королевы, короля, пешки, ладьи, коня, слона
 		// от этих фигур наследуют их клоны, для черных и для белых со своими позициями. Затем они расставляются.
 		// Затем уже отображаются. При ходе будут сравниваться две фигуры.
@@ -32,6 +56,19 @@
 
 			defends () { return false; }
 			check () { return false; }
+
+			test (x, y) {
+				if ((field[y][x].color == undefined) || (field[y][x].color != this.color)) {
+					var prev_figure = new field[y][x].constructor(field[y][x]);
+					var [tx, ty] = [this.x, this.y];
+					this.turn(x, y);
+					var res = check_union();
+					this.turn(tx, ty);
+					field[y][x] = new prev_figure.constructor(prev_figure);
+					return !res;
+				}
+				else return false;
+			}
 
 			get table (){
 				var table = [];
@@ -78,7 +115,7 @@
 		class King extends Figure {
 			constructor (settings) {
 				super(settings);
-				this.first_turn = true;
+				this.first_turn = settings.first_turn;
 			}
 			turn (x, y) { // своя ф-ция хода, ибо ракировка
 				super.turn(x, y);
@@ -89,7 +126,7 @@
 					for (var i = 0; i < 8; i++) 
 						for (var j = 0; j < 8; j++) 
 							if (!((i == this.y) && (j == this.x)))
-								if ((field[i][j].color != this.color) && field[i][j].table[y][x])
+								if ((field[i][j].color != this.color) && field[i][j].defends(x, y))
 									return false;
 					return (field[y][x].color != this.color) || (field[y][x].color == undefined);
 				}
@@ -101,17 +138,19 @@
 				else return false;
 			}
 		}
+
 		class Queen extends Figure {
 			check (x, y) {
 				if (this.x == x) {
 					for (var i = this.y - Math.sign(this.y-y); i != y; i -= Math.sign(this.y-y))
 						if (field[i][x].color != undefined) return false;
-					return (field[i][x].color == undefined) || (field[i][x].color != this.color)
+					if (field[y][x] instanceof King) return true;
+					else return this.test(x, y);
 				}
 				else if (this.y == y) {
 					for (var i = this.x - Math.sign(this.x-x); i != x; i -= Math.sign(this.x-x))
 						if (field[y][i].color != undefined) return false;
-					return (field[y][i].color == undefined) || (field[y][i].color != this.color)
+					return this.test(x, y);
 				}
 				else if (Math.abs(this.y - y) == Math.abs(this.x - x)) {
 					var i = this.y - Math.sign(this.y-y);
@@ -121,7 +160,7 @@
 						i -= Math.sign(this.y - y);
 						j -= Math.sign(this.x - x);
 					}
-					return (field[y][x].color == undefined) || (field[y][x].color != this.color);
+					return this.test(x, y);
 				}
 				else return false;
 			}
@@ -154,22 +193,22 @@
 		class Rook extends Figure {
 			constructor (settings) {
 				super(settings);
-				this.first_turn = true;
+				this.first_turn = settings.first_turn;
 			}
 			turn (x, y) {
 				super.turn(x, y);
-				if (this.first_turn) this.first_turn = true;
+				if (this.first_turn) this.first_turn = false;
 			}
 			check (x, y) {
 				if (this.x == x) {
 					for (var i = this.y - Math.sign(this.y-y); i != y; i -= Math.sign(this.y-y))
 						if (field[i][x].color != undefined) return false;
-					return (field[i][x].color == undefined) || (field[i][x].color != this.color)
+					return this.test(x, y);
 				}
 				else if (this.y == y){
 					for (var i = this.x - Math.sign(this.x-x); i != x; i -= Math.sign(this.x-x))
 						if (field[y][i].color != undefined) return false;
-					return (field[y][i].color == undefined) || (field[y][i].color != this.color)
+					return this.test(x, y);
 				}
 				else return false;
 			}
@@ -199,7 +238,7 @@
 						i -= Math.sign(this.y - y);
 						j -= Math.sign(this.x - x);
 					}
-					return (field[y][x].color == undefined) || (field[y][x].color != this.color);
+					return this.test(x, y);
 				}
 				else return false;
 			}
@@ -223,7 +262,7 @@
 			check (x, y) {
 				if (((Math.abs(this.x - x) == 2) && (Math.abs(this.y - y) == 1)) ||
 					((Math.abs(this.x - x) == 1) && (Math.abs(this.y - y) == 2))) 
-					return (field[y][x].color == undefined) || (field[y][x].color != this.color);
+					return this.test(x, y);
 				else return false;
 			}
 			defends (x, y) {
@@ -236,7 +275,7 @@
 		class Pawn extends Figure {
 			constructor (settings) {
 				super(settings);
-				this.first_turn = true;
+				this.first_turn = settings.first_turn;
 			}
 			turn(x, y) {
 				super.turn(x, y);
@@ -267,16 +306,16 @@
 
 		// записывать буду сразу на свои позиции
 
-		field[7][4] = new King({ x: 4, y: 7, color: true, src: 'img/wK.png' }); // белый король
-		field[0][4] = new King({ x: 4, y: 0, color: false, src: 'img/bK.png' }); // черный король
+		field[7][4] = new King({ x: 4, y: 7, color: true, src: 'img/wK.png', first_turn: true }); // белый король
+		field[0][4] = new King({ x: 4, y: 0, color: false, src: 'img/bK.png', first_turn: true }); // черный король
 
 		field[7][3] = new Queen({ x: 3, y: 7, color: true, src: 'img/wQ.png' }); // белый ферзь
 		field[0][3] = new Queen({ x: 3, y: 0, color: false, src: 'img/bQ.png' }); // черный ферзь
 
-		field[7][7] = new Rook({ x: 7, y: 7, color: true, src: 'img/wR.png' }); // белая ладья_1
-		field[7][0] = new Rook({ x: 0, y: 7, color: true, src: 'img/wR.png' }); // белая ладья_2
-		field[0][7] = new Rook({ x: 7, y: 0, color: false, src: 'img/bR.png' }); // черная ладья_1
-		field[0][0] = new Rook({ x: 0, y: 0, color: false, src: 'img/bR.png' }); // черная ладья_2
+		field[7][7] = new Rook({ x: 7, y: 7, color: true, src: 'img/wR.png', first_turn: true }); // белая ладья_1
+		field[7][0] = new Rook({ x: 0, y: 7, color: true, src: 'img/wR.png', first_turn: true }); // белая ладья_2
+		field[0][7] = new Rook({ x: 7, y: 0, color: false, src: 'img/bR.png', first_turn: true }); // черная ладья_1
+		field[0][0] = new Rook({ x: 0, y: 0, color: false, src: 'img/bR.png', first_turn: true }); // черная ладья_2
 
 		field[7][2] = new Bishop({ x: 2, y: 7, color: true, src: 'img/wB.png' }); // белый слон_1
 		field[7][5] = new Bishop({ x: 5, y: 7, color: true, src: 'img/wB.png' }); // белый слон_2
@@ -289,8 +328,8 @@
 		field[0][6] = new Horse({ x: 6, y: 0, color: false, src: 'img/bN.png' }); // черный конь_2
 
 		for (var i = 0; i < 8; i++) {
-			field[6][i] = new Pawn({ x: i, y: 6, color: true, src: 'img/wP.png' }); // белая пешка
-			field[1][i] = new Pawn({ x: i, y: 1, color: false, src: 'img/bP.png' }); // черная пешка
+			field[6][i] = new Pawn({ x: i, y: 6, color: true, src: 'img/wP.png', first_turn: true }); // белая пешка
+			field[1][i] = new Pawn({ x: i, y: 1, color: false, src: 'img/bP.png', first_turn: true }); // черная пешка
 		}
 
 		// initializating field
@@ -305,23 +344,10 @@
 
 		var order_now = true; // true == ход белых
 		var figure_chosen = undefined; // undefined - фигура не выбрана
-		var checked = false;
-
-		function find_king () {
-			for (var i = 0; i < 8; i++)
-				for (var j = 0; j < 8; j++)
-					if ((field[i][j] instanceof King) && (field[i][j].color != order_now)) 
-						return {x: j, y: i};
-		}
-
-		function check() {
-			var kp = find_king();
-			return field.some( (a) => a.some( (b) => b.check(kp.x, kp.y) ) );
-		}
 
 		function checkmate() {
-			var kp = find_king();
-			if (check()) 
+			var kp = find_enemy_king();
+			if (check_enemy()) 
 				if (field[kp.y][kp.x].table_turns.every((a) => a.every((b) => !b))) {
 					var arr = [];
 					for (var i = 0; i < 8; i++) 
@@ -385,7 +411,6 @@
 						figure_chosen.turn(x, y);
 						if (checkmate())
 							alert(`${order_now ? 'Белые' : 'Черные'} победили!`);
-						else if (check()) {  }
 						order_now = !order_now;
 						figure_chosen = undefined;
 					}
